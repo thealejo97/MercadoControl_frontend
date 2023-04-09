9dd5f9981813c0e11aaeebdaccc395678b507f44
Andrés Mauricio Rodríguez Zúñiga <45635241+andresmrz@users.noreply.github.com>
2023-04-09 15:23:39 -0500

    Delete MercadoControl_frontend directory

--- MercadoControl_frontend/api.js
+++
@@ -1,9 +0,0 @@
-export const fetchData = async () => {
-    try {
-      const response = await fetch('https://mercadocontrolback.fly.dev/api/users/');
-      const data = await response;console.log(data);
-      return data.json();
-    } catch (error) {
-      console.error(error);
-    }
-  };

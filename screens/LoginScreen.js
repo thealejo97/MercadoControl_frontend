e312d1d049ca058ebe9e3974f3e0469aba3d16f6
Alejandro Montano <alejandromontano@useit.co>
2023-04-12 18:04:19 -0500

    adding login api logic

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,12 +1,54 @@
 import { View, Text, TouchableOpacity, Image, TextInput } from 'react-native'
-import React from 'react'
+import React,{ useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import {ArrowLeftIcon} from 'react-native-heroicons/solid'
 import { themeColors } from '../theme'
 import {styles} from '../theme/styles'
 import { useNavigation } from '@react-navigation/native'
 
+
+
 export default function LoginScreen() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [csrfToken, setCsrfToken] = useState('');
+
+  const handleLogin = async () => {
+    /*
+     * Login view, allows to the user login with the django backend.
+
+        How it works:
+        Initially, we request the CSRF token from the Django backend, and then we use that token to check if the data is correct or incorrect.
+
+        Created by: Alejandro Montaño
+        Date: 11-04-2023
+     */
+
+    const csrfResponse = await fetch('https://mercadocontrolback.fly.dev/api/users/csrf_cookie/');
+    const token = csrfResponse.headers.get('Set-Cookie').split('=')[1].split(';')[0];
+    setCsrfToken(token);
+
+    const response = await fetch('https://mercadocontrolback.fly.dev/api/users/login/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-CSRFToken': token,
+      },
+      body: JSON.stringify({
+        username,
+        password,
+      }),
+    });
+
+    const data = await response.json();
+
+    if (response.ok) {
+      console.log('si');
+    } else {
+      console.log('No');
+    }
+  };
+
   const navigation = useNavigation();
   return (
     <View className="flex-1 bg-white" style={{backgroundColor: themeColors.bg}}>
@@ -32,14 +74,16 @@ export default function LoginScreen() {
             <TextInput 
               className="p-4 bg-gray-100 text-gray-700 rounded-2xl mb-3"
               placeholder="email"
-              value="Puto el que lo lea" 
+              value={username}
+              onChangeText={setUsername}
             />
             <Text className="text-gray-700 ml-4" style={[]}>Password</Text>
             <TextInput 
               className="p-4 bg-gray-100 text-gray-700 rounded-2xl" style={[]}
               secureTextEntry
               placeholder="password"
-              value="test12345" 
+              value={password}
+              onChangeText={setPassword}
             />
             <TouchableOpacity className="flex items-end" style={[]}>
               <Text className="text-gray-700 mb-5" style={[]}>Forgot Password?</Text>
@@ -47,10 +91,11 @@ export default function LoginScreen() {
             <TouchableOpacity 
               className="py-3 bg-yellow-400 rounded-xl" style={[]}>
                 <Text 
-                    className="text-xl font-bold text-center text-gray-700" style={[]}
+                    className="text-xl font-bold text-center text-gray-700" style={[]} onPress={handleLogin} 
                 >
                         Login
                 </Text>
+                
              </TouchableOpacity>
             
           </View>
